8f54de771af83adaafc36b38a1862c725989db6f
alejandro <alejandroreyes.dig@gmail.com>
2024-03-26 19:11:02 -0600

    Proyecto6

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 #servidor
-URL_SERVICE = "https://cnt-97d29609-d989-4336-956d-7f81a567ec95.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-5387aaf2-9bb1-4ef3-ab5e-a68bc013b28c.containerhub.tripleten-services.com"
 
 #endpoint para crear un nuevo usuario
 CREATE_USER_PATH = "/api/v1/users/"

4e31d211af1373e651f2dd7eb86ef516cb083120
alejandro <alejandroreyes.dig@gmail.com>
2024-03-31 14:03:04 -0600

    Puse todos los datos de data.py que incluyan el campo name y elimine la función get_kit_body() en create_kit_name_kit_test

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 #servidor
-URL_SERVICE = "https://cnt-6b8376f9-cf1d-444e-9e49-30d64d31aad6.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-95796aba-b899-45a3-8f86-c092d5c6f67e.containerhub.tripleten-services.com"
 #endpoint para crear un nuevo usuario
 CREATE_USER_PATH = "/api/v1/users/"
 #endpoint para crear un nuevo kit
